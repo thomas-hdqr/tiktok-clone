42d2bf608ac2d3d6d5e22b5c5342642e8a32f131
Thomas <thomashaudiquer@gmail.com>
2022-08-09 11:42:06 +0100

    installing firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB-plghlLfdctzVEWi2HH_g_Gp3VSnwI74",
+    authDomain: "tiktok-clone-694f0.firebaseapp.com",
+    projectId: "tiktok-clone-694f0",
+    storageBucket: "tiktok-clone-694f0.appspot.com",
+    messagingSenderId: "533337314441",
+    appId: "1:533337314441:web:fb9df2805cc781f40f6258",
+    measurementId: "G-CX38HDC42F"
+  };
