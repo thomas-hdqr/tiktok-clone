7480bb4bb5469cf46e513de1acb1e50fb73764cc
Thomas <thomashaudiquer@gmail.com>
2022-08-10 13:02:36 +0100

    building the footer component

--- src/Video.js
+++ src/Video.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react'
 import './Video.css'
+import VideoFooter from './VideoFooter'
 
 function Video() {
     const [playing, setPlaying] = useState(false)
@@ -24,7 +25,7 @@ function Video() {
         ref={videoRef}
         controls
         src="https://v16-webapp.tiktok.com/35ca7e969d05916d3a637dcdc721ccb0/62f3de44/video/tos/useast2a/tos-useast2a-pve-0068/83e11de2103245dbbbf692d1a7cd75ab/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=3180&bt=1590&btag=80000&cs=0&ds=3&ft=eXd.6HKSMyq8ZRAO_we2N1_Aol7Gb&mime_type=video_mp4&qs=0&rc=OzgzOzZmNzM1OGQ1N2Y3NUBpM3Brczd3b2c0eDMzOjczM0A0MDRjM2ExXjExXjEzNmJiYSNhai5iLWhvNGlfLS01MTZzcw%3D%3D&l=2022081010345001021702913505023F3C"></video>
-        {/* <VideoFooter /> */}
+        <VideoFooter />
         {/* <VideoSidebar /> */}
     </div>
   )
---
+++ src/VideoFooter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VideoFooter() {
+  return (
+    <h2>I am a footer</h2>
+  )
+}
+
+export default VideoFooter
