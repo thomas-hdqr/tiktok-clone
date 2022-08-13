1b9ebf50b89b1b4154fe35ba1366f6eca2f04d37
Thomas <thomashaudiquer@gmail.com>
2022-08-13 15:39:21 +0100

    passing props to the video component

--- src/Video.js
+++ src/Video.js
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from 'react'
 import './Video.css'
 import VideoFooter from './VideoFooter'
+import VideoSidebar from './VideoSidebar'
 
-function Video() {
+function Video({ url, channel, description, song, likes, messages, shares}) {
     const [playing, setPlaying] = useState(false)
     const videoRef = useRef(null)
 
@@ -23,9 +24,9 @@ function Video() {
         loop
         onClick={onVideoPress}
         ref={videoRef}
-        src="https://v16-webapp.tiktok.com/6c2829cd322e6808594403dac5c9e5ef/62f7ce16/video/tos/alisg/tos-alisg-pve-0037c001/fecd465dcecc43d4847e84a0455be1a6/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=5166&bt=2583&cs=0&ds=3&ft=eXd.6HKSMyq8Zp2lHwe2NkFTyl7Gb&mime_type=video_mp4&qs=0&rc=NDc4N2RmMzk5PDc7aGQ7aUBpandldjU6Zm88ZDMzODczNEAyM2AxYjQzNmAxMTFfYDBhYSNoNmEucjRvX3BgLS1kMS1zcw%3D%3D&l=202208131014190102231230142040FB17&btag=80000"></video>
-        <VideoFooter />
-        {/* <VideoSidebar /> */}
+        src={url}></video>
+        <VideoFooter channel={channel} description={description} song={song} />
+        <VideoSidebar likes= {likes} messages={messages} shares={shares}/>
     </div>
   )
 }
--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -3,26 +3,30 @@ import './VideoFooter.css'
 import MusicNoteIcon from '@mui/icons-material/MusicNote';
 import Ticker from 'react-ticker'
 
-function VideoFooter() {
+function VideoFooter({ channel, description, song }) {
   return (
     <div className="videoFooter">
-        <div className="videoFooter__text">
-            <h3>@tiktok</h3>
-            <p>this is a description</p>
-            <div className="videoFooter__ticker">
-                <MusicNoteIcon className="videoFooter__icon"/>
-                <Ticker mode="smooth">
-                    {({ index }) => (
-                    <>
-                        <p>Yo whats up guys</p>
-                    </>
-                    )}
-                </Ticker>
-            </div>
+      <div className="videoFooter__text">
+        <h3>@{channel}</h3>
+        <p>{description}</p>
+        <div className="videoFooter__ticker">
+          <MusicNoteIcon className="videoFooter__icon" />
+          <Ticker mode="smooth">
+            {({ index }) => (
+              <>
+                <p>{song}</p>
+              </>
+            )}
+          </Ticker>
         </div>
-        <img className="videoFooter__record" src="https://static.thenounproject.com/png/934821-200.png" alt="" />
+      </div>
+      <img
+        className="videoFooter__record"
+        src="https://static.thenounproject.com/png/934821-200.png"
+        alt=""
+      />
     </div>
-  )
+  );
 }
 
-export default VideoFooter
+export default VideoFooter;
