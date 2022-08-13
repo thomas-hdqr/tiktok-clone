a860764b999c8bd6bed629a2be9623f159fb4f77
Thomas <thomashaudiquer@gmail.com>
2022-08-13 12:52:42 +0100

    working on footer

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './VideoFooter.css'
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import Ticker from 'react-ticker'
 
 function VideoFooter() {
   return (
     <div className="videoFooter">
-        <h2>I am a footer</h2>
+        <div className="videoFooter__text">
+            <h3>@tiktok</h3>
+            <p>this is a description</p>
+            <div className="videoFooter__ticker">
+                <MusicNoteIcon className="videoFooter__icon"/>
+                <Ticker mode="smooth">
+                    {({ index }) => (
+                    <>
+                        <p>Yo whats up guys</p>
+                    </>
+                    )}
+                </Ticker>
+            </div>
+        </div>
+        <img className="videoFooter__record" src="https://static.thenounproject.com/png/934821-200.png" alt="" />
     </div>
   )
 }
