ea0c9d723cb03ae25886c9145ea66314fddd7b84
Thomas <thomashaudiquer@gmail.com>
2022-09-08 15:57:16 +0100

    fixing a tiny bug with millify

--- README.md
+++ README.md
@@ -3,9 +3,7 @@ Building a clone of TikTok with React.js
 Next tasks:
 - add minify to compress number in k and m
 - fix the likes button (currently add 1 next to number of likes)
-- fix the song text
 - add an autoplay with song? 
 - create a local database and map all the data
 - check the responsive part 
-- deploy on netlify
 
--- src/App.js
+++ src/App.js
@@ -14,33 +14,33 @@ function App() {
             description="This is the reality of my life... #programmer #programming #coding #code #coder #softwareengineer #computerscience #tiktoktrend #developer #codinglife"
             song="Cursed renegade - AVE"
             likes={millify(1263293)}
-            message={millify(239)}
-            share={millify(231)}
+            messages={millify(239)}
+            shares={millify(231)}
             />
             <Video
             url={lisbon}
             channel="wheretofindme"
             description="if you’re planning a trip to Portugal, this fairytale location is a must visit! #sintra #portugaltiktok #tiktokportugal #lisbon"
             song="Une barque sur l'océan from Miroirs - Andre Laplante"
-            like={millify(20254882)}
-            message={millify(298)}
-            share={millify(238)}
+            likes={millify(20254882)}
+            messages={millify(298)}
+            shares={millify(238)}
             />
             <Video
             url={bjj}
             channel="smittywit"
             description="This Is What Makes Jiu-Jitsu Fun"
             song="Piano Lofi Hiphop(1087796) - KEYAG"
-            like={millify(345632)}
-            message={millify(3167)}
-            share={millify(626)}
+            likes={millify(345632)}
+            messages={millify(3167)}
+            shares={millify(626)}
             />
             <Video
             url={biking}
             channel="cycleclub1"
             description="It's too crazy#bike #foryou #sports #fyp #crazy #cycling #mountainbike"
             song="original sound - Cycling Club"
-            like={millify(234737)}
+            likes={millify(234737)}
             messages={millify(1533)}
             shares={millify(1241)}
             />
@@ -50,7 +50,7 @@ function App() {
             description="La pizza Italy…🇮🇹 #pizzatime #pizza #pizzanapoletana #felicità"
             song="suono originale - Aurelio Visciotti"
             likes={millify(302000)}
-            messages={millify(2550)}
+            messages={millify(26550)}
             shares={millify(2500)}
             />
       </div>
